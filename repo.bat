a47b7908f65b68d600f1798102adae303a790bac
rincoreiys <febri7299@gmail.com>
2022-08-05 15:22:17 +0700

    PUSH

--- repo.bat
+++ repo.bat
@@ -2,10 +2,11 @@
 echo off
 set action=%1
 
+@REM git remote add origin https://github.com/rincoreiys/CS_CLIENT.git
 IF "%action%"=="push" (
     git add --all
     git commit -m "PUSH"
-    git remote add origin https://github.com/rincoreiys/CS_CLIENT.git
+    
     git push
 )
 
